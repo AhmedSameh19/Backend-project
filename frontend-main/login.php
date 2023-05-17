eacb3d55534713f947380c29f07b32f1b103d339
Ahmed Sameh <105107228+AhmedSameh19@users.noreply.github.com>
2023-05-17 12:53:58 +0300

    login

--- frontend-main/login.php
+++ frontend-main/login.php
@@ -1,26 +1,33 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Retrieve form data
-    $phone = $_POST['phone'];
-    $password = $_POST['password'];
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body class="container">
+  <div class="header">
+  	<h2>Login</h2>
+  </div>
 
-    // Validate form data (you can add more validation if required)
-    if (empty($phone) || empty($password)) {
-        // Handle form validation errors
-        echo "Please fill in all fields.";
-        exit;
-    }
-
-    // Perform login process (replace this with your actual login logic)
-    // Example: check if the provided phone number and password match a user in the database
-    if ($phone === 'user@example.com' && $password === 'password') {
-        // Login successful
-        echo "Login successful!"; // You can redirect the user to a different page or perform other actions here
-        exit;
-    } else {
-        // Login failed
-        echo "Invalid phone number or password.";
-        exit;
-    }
-}
-?>
+  <form method="post" action="login.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  		<label>Username</label>
+  		<input type="text" name="username" >
+  	</div>
+  	<div class="input-group">
+  		<label>Password</label>
+  		<input type="password" name="password">
+  	</div>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="login_user">Login</button>
+  	</div>
+  	<p>
+  		Craete an account  <a href="register.php">Sign up</a>
+  	</p>
+  </form>
+</body>
+</html>
